0cef5363f1e23743d7e65c84db3f01be6107ab6f
Zoiad712 <a2338847@tamu.edu>
2025-09-16 20:13:48 -0500

    make_change.py

--- Make_change.py
+++ Make_change.py
@@ -9,38 +9,39 @@
 # Xander Tivis
 # Section: ENGR - 102 - 559
 # Assignment: Lab 4 Team project planner
-# Date: 16-09-2025        
+# Date: 16-09-2025
+         
 
 # This program will find how many an which coins are needed to be given as change    
 
 from math import *
 
-# gets input from user
-pay = float(input("How much did you pay ($): "))
-change = float(input("How much did it cost ($): "))
-totalchange = pay - change
+# gets inputs from user
+pay = float(input("How much did you pay? "))
+cost = float(input("How much did it cost? "))
 
-# checks to see if the amount should be paid in coins
-if totalchange >= 1:
-    print("\nThis amount should not be paid with coins")
-    findcoins = round(totalchange * 100)
-elif totalchange < 1 and totalchange > 0:
-    findcoins = round(totalchange * 100)
-else:
-    print("\nThis amount should not be paid with coins")
-    findcoins = int(totalchange)
+# finds how much change should be given
+change = round(pay - cost, 2)
 
-quarters = findcoins // 25 # finds the amount of quarters needed 
-findcoins %= 25 # finds the amount of change remaining after removing the amount of quarters
-dimes = findcoins // 10 # finds the amount of dimes from remaining change
-findcoins %= 10 # finds the amount of change remaining after removing the amount of dimes
-nickels = findcoins // 5 # finds the amount of nickels from remaining change
-findcoins %= 5 # finds the amount of change remaining after removing the amount of nickels
-pennies = findcoins # gives the final amount of pennies
+# converts the amount of change to whole numbers for easy floor division and modulus
+coins = int(round(change * 100))
 
-totalchange = round(totalchange,2)
-# prints all of the quantities owed
-print(f"\nYou should recieve a total of {totalchange} in the form of:\n{quarters} quarter(s)\n{dimes} dime(s)\n{nickels} nickel(s)\n{pennies} pennie(s)")
+quarters = coins // 25 # finds the amount of quarters needed 
+coins %= 25 # finds the amount of change remaining after removing the amount of quarters
+dimes = coins // 10 # finds the amount of dimes from remaining change
+coins %= 10 # finds the amount of change remaining after removing the amount of dimes
+nickels = coins // 5 # finds the amount of nickels from remaining change
+coins %= 5 # finds the amount of change remaining after removing the amount of nickels
+pennies = coins # gives the final amount of pennies
 
+print(f"You received ${change} in change. That is...")
 
-
+# checks to see if the amount of coins are > 0 and if they are > 1 and prints appropriately
+if quarters > 0:
+    print(f"{quarters} quarter" + ("s" if quarters > 1 else ""))
+if dimes > 0:
+    print(f"{dimes} dime" + ("s" if dimes > 1 else ""))
+if nickels > 0:
+    print(f"{nickels} nickel" + ("s" if nickels > 1 else ""))
+if pennies > 0:
+    print(f"{pennies} penn" + ("ies" if pennies > 1 else "y"))
