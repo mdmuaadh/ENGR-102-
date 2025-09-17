a991b43acbb9c3425413fb8e8eb1c497787307c4
Zoiad712 <a2338847@tamu.edu>
2025-09-16 20:16:50 -0500

    make_change.py

--- Make_change.py
+++ Make_change.py
@@ -34,7 +34,7 @@ nickels = coins // 5 # finds the amount of nickels from remaining change
 coins %= 5 # finds the amount of change remaining after removing the amount of nickels
 pennies = coins # gives the final amount of pennies
 
-print(f"You received ${change} in change. That is...")
+print(f"You received ${change:.2f} in change. That is...")
 
 # checks to see if the amount of coins are > 0 and if they are > 1 and prints appropriately
 if quarters > 0:
@@ -45,3 +45,4 @@ if nickels > 0:
     print(f"{nickels} nickel" + ("s" if nickels > 1 else ""))
 if pennies > 0:
     print(f"{pennies} penn" + ("ies" if pennies > 1 else "y"))
+
