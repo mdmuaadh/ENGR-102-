7c2c17b3a1bb0ee0405ab6d7bcd6cc63bebe838c
Zoiad712 <a2338847@tamu.edu>
2025-10-07 20:17:07 -0500

    go_move.py

--- go_move.py
+++ go_move.py
@@ -49,14 +49,15 @@ turn_black = True
 print("Type coordinates like 'row col' (for example: 3 5) or type 'stop' to end.")
 print_board()
 
-#
 while True:
+    #Checks which players turn it is and saves which tile should be used
     if turn_black:
         stone = 'O'
         player = "Black"
     else:
         stone = 'o'
         player = "White"
+    #Collects the users input and checks whether it is valid or will end the game
     move = input(f"{player}'s move (row col) or 'stop': ")
     if move.lower() == "stop":
         print("Game ended")
@@ -65,17 +66,21 @@ while True:
     if len(parts) != 2:
         print("Please enter row and column numbers separated by a space.")
         continue
+    #defines which row and column the users input is
     row = int(parts[0]) - 1
     col = int(parts[1]) - 1
+    #checks to see if users input fits on the board
     if row < 0 or row > 8 or col < 0 or col > 8:
         print("Coordinates out of range! Use numbers 1–9.")
         continue
+    #checks to see if the tile is empty or not and reports an error if it is full
     if board[row][col] != empty:
         print("That spot is already taken. Try again.")
         continue
 
+    #adds the users input to the board
     board[row][col] = stone
+    #prints the new board
     print_board()
+    #switches turns
     turn_black = not turn_black
-
-
